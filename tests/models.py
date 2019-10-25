aeb43bcf61e95d3f419b7ddbcd4ce9464f80a3f5
Charles Leifer <coleifer@gmail.com>
2019-10-24 20:15:11 -0500

    Fixups, specific test for crdb view sql.

--- tests/models.py
+++ tests/models.py
@@ -20,6 +20,7 @@ from .base import requires_sqlite
 from .base import skip_if
 from .base import skip_unless
 from .base import BaseTestCase
+from .base import IS_CRDB
 from .base import IS_MYSQL
 from .base import IS_MYSQL_ADVANCED_FEATURES
 from .base import IS_POSTGRESQL
@@ -2226,6 +2227,7 @@ class TestForUpdateIntegration(ModelTestCase):
             self.database.commit()
             self.assertEqual(query.get().username, 'ziggy')
 
+    @skip_if(IS_CRDB, 'crdb does not support "nowait" option')
     def test_for_update_nowait(self):
         User.create(username='huey')
         zaizee = User.create(username='zaizee')
